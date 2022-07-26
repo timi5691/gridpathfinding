8da58f770a3d25ee06fe8e5302f127a2be8deefc
timi5691 <39525652+timi5691@users.noreply.github.com>
2022-07-26 11:02:42 +0700

    Update grid_path_finding.v

--- grid_path_finding/grid_path_finding.v
+++ grid_path_finding/grid_path_finding.v
@@ -1,6 +1,15 @@
+/////////////////////////////////////////////
+/// GRID AND PATHFINDING
 module grid_path_finding
 
 import math {sqrt}
+
+pub struct GridCell {
+pub mut:
+	pos GridPos
+	pixelpos PixelPos
+	walkable bool = true
+}
 
 pub struct GridData {
 pub mut:
@@ -18,18 +27,9 @@ pub mut:
 
 pub struct PixelPos {
 pub mut:
-	x int
-	y int
+	x f32
+	y f32
 }
-
-pub struct GridCell {
-pub mut:
-	pos GridPos
-	pixelpos PixelPos
-	walkable bool = true
-}
-
-
 
 struct Cost {
 mut:
@@ -85,9 +85,14 @@ pub fn (data GridData) gridpos_to_cell_id(pos GridPos) int {
 	return pos.row*data.cols + pos.col
 }
 
-pub fn (data GridData) get_id_from_pixel_pos(x int, y int) int {
-	col := x/data.cell_size
-	row := y/data.cell_size
+pub fn (data GridData) cell_id_to_pixelpos(cell_id int) PixelPos {
+	grpos := data.cell_id_to_gridpos(cell_id)
+	return data.gridpos_to_pixel_pos(grpos)
+}
+
+pub fn (data GridData) get_id_from_pixel_pos(x f32, y f32) int {
+	col := int(x/data.cell_size)
+	row := int(y/data.cell_size)
 	return row*data.cols + col
 }
 
@@ -140,9 +145,8 @@ fn (data GridData) calc_cost(a int, b int, optimized bool) int {
 	dx := myabs(b_pos.row - a_pos.row)
 	if optimized{
 		return if dx == dy {2*dx} else {dx + dy}
-	} else {
-		return int(sqrt(dx*dx + dy*dy)) // if not optimized it will use sqrt func to calculate cost of cells
 	}
+	return int(sqrt(dx*dx + dy*dy))
 }
 
 
@@ -164,14 +168,24 @@ fn find_best_cost(open map[int]Cost) int {
 }
 
 
-fn calculate_path(current int, start int, parents map[int]int) []int {
+fn (data GridData) calculate_path(current int, start int, parents map[int]int) []PixelPos {
+	half_cell_size := data.cell_size/2
 	mut p := current
-	mut path := []int{}
+	mut pos := data.cell_id_to_pixelpos(p)
+	pos.x += half_cell_size
+	pos.y += half_cell_size
+	mut path := []PixelPos{}
+	mut idpath := []int{}
 	for p != start {
-		path.prepend(p)
+		path.prepend(pos)
+		idpath.prepend(p)
 		p = parents[p]
+		pos = data.cell_id_to_pixelpos(p)
+		pos.x += half_cell_size
+		pos.y += half_cell_size
 	}
-	path.prepend(p)
+	idpath.prepend(p)
+	path.prepend(pos)
 	return path
 }
 
@@ -185,7 +199,7 @@ fn (data GridData) remove_not_walkable_neighbor (mut neighbors []int) {
 	}
 }
 
-pub fn (data GridData) path_finding(start int, end int, optimized bool) []int {
+pub fn (data GridData) path_finding(start int, end int, optimized bool) []PixelPos {
 	mut open := map[int]Cost{}
 	mut closed := map[int]Cost{}
 	mut parents := map[int]int{}
@@ -198,24 +212,25 @@ pub fn (data GridData) path_finding(start int, end int, optimized bool) []int {
 	}
 
 	mut current := start
-	mut path := []int{}
+	start_pos := data.cell_id_to_pixelpos(current)
+	mut path := []PixelPos{}
 
 	for open.len != 0 {
 		current = find_best_cost(open)
 		if current == end {
-			path = calculate_path(current, start, parents)
+			path = data.calculate_path(current, start, parents)
 			return path
 		}
 		neighbors := data.get_neighbor_ids(current)
 		for neighbor in neighbors {
 			to_start_now := open[current].to_start + 1
-			if _ := open[neighbor] {// replace (neighbor in to_research) for a little more performance
+			if _ := open[neighbor] {
 				if open[neighbor].to_start > to_start_now {
 					open[neighbor].to_start = to_start_now
 					open[neighbor].total = to_start_now + open[neighbor].to_end
 					parents[neighbor] = current
 				}
-			} else if _ := closed[neighbor] {// replace (neighbor in closed) for a little more performance
+			} else if _ := closed[neighbor] {
 				if closed[neighbor].to_start > to_start_now {
 					closed[neighbor].to_start = to_start_now
 					closed[neighbor].total = to_start_now + closed[neighbor].to_end
@@ -238,7 +253,88 @@ pub fn (data GridData) path_finding(start int, end int, optimized bool) []int {
 		open.delete(current)
 	}
 	if current != end{
-		path = [start]
+		path = [start_pos]
 	}
 	return path
+}
+//////////////////////////////////////////////////////////////////////////////////
+/// FOLLOW PATH
+
+pub struct PathFollower {
+pub mut:
+	path []PixelPos
+	pos PixelPos
+	status int // 0 mean stop, 1 mean follow path
+	spd f32 = 0.1 // from 0.0 to 1.0
+	t f32 // from 0.0 to 1.0
+	a f32
+	b f32
+	step int
+	after_finished_do int // 0 mean stop, 1 mean repeat, 2 mean reverse
+}
+
+pub fn (mut fl PathFollower) set_path(pth_of_pos []PixelPos, grid_data GridData) {
+	fl.path = pth_of_pos
+	if fl.pos != fl.path[0] {
+		fl.path[0] = fl.pos
+	}
+}
+
+pub fn (mut fl PathFollower) start_move() {
+	fl.t = 0
+	fl.step = 0
+	fl.status = 1
+}
+
+pub fn (mut fl PathFollower) moving() bool {
+	if fl.status != 1 || fl.path.len < 2 {
+		return false
+	}
+
+	pth_size := fl.path.len
+	if fl.step == pth_size - 1 {
+		fl.finished_move()
+		return false
+	}
+
+	x0 := fl.path[fl.step].x
+	y0 := fl.path[fl.step].y
+	x1 := fl.path[fl.step + 1].x
+	y1 := fl.path[fl.step + 1].y
+
+	fl.a = x1 - x0
+	fl.b = y1 - y0
+
+	fl.pos.x = x0 + fl.a*fl.t
+	fl.pos.y = y0 + fl.b*fl.t
+	
+	fl.t += fl.spd
+	if fl.t >= 1 {
+		fl.t = 0
+		fl.step += 1
+	}
+
+	return true
+}
+
+fn (mut fl PathFollower) finished_move() {
+	pth_size := fl.path.len
+	match fl.after_finished_do {
+		// stop
+		0 { 
+			fl.pos = fl.path[pth_size - 1]
+			fl.status = 0
+		}
+		// repeat
+		1 {
+			fl.pos = fl.path[0]
+			fl.start_move()
+		}
+		// reverse
+		2 {
+			fl.path = fl.path.reverse()
+			fl.start_move()
+			}
+		else {}
+	}
 }
